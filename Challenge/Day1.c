8df4243fb3a05fbdf0a9e971f0aaa4ab90097ce3
dduck00 <ejr075@gmail.com>
2020-03-09 22:21:12 +0900

    Update Day1.c
    조합알고리즘 변경

--- Challenge/Day1.c
+++ Challenge/Day1.c
@@ -3,69 +3,75 @@
 #include <stdlib.h>
 #include <string.h>
 
-char max[1000000] = {0};
-char tempAns[1000000] = {0};
-int cutPosition[1000000] = {0};
+char maxNum[1000000] = { 0 };
+char compNum[1000000] = { 0 };
+char* numberPointer;
+
+int cutPosition[1000000] = { 0 };
 int maxDepth;
 int targetDepth;
+int ansLength;
 
-int diff()
+int isTempAnsBig()
 {
-    for (int idx = 0; idx < maxDepth - targetDepth; idx++)
-    {
-        if (max[idx] == tempAns[idx])
-            continue;
-        return max[idx] < tempAns[idx];
-    }
+	for (int idx = 0; idx < ansLength; idx++)
+	{
+		if (maxNum[idx] == compNum[idx])
+			continue;
+		return maxNum[idx] < compNum[idx];
+	}
 }
 
-void makeTempAns(const char *num)
+void makeTempAns(const char* num)
 {
-    int count = 0;
-    for (int idx = 0; idx < maxDepth - targetDepth; idx++)
-    {
-        if (cutPosition[count] == 0)
-        {
-            tempAns[idx] = num[count];
-            count++;
-        }
-        else
-        {
-            count++;
-            idx--;
-        }
-    }
+	int count = 0;
+	int realIdx = 0;
+	for (int idx = 0; idx < ansLength; idx++)
+	{
+		if (count >= maxDepth)
+			break;
+
+		if (cutPosition[count] != idx+count)
+		{
+			compNum[idx] = num[idx+count];
+		}
+		else
+		{
+			count++;
+			idx--;
+		}
+	}
 }
 
-void dept(const char *num, int start, int rank)
+void dept(int index, int rank, int position)
 {
-    if (rank == targetDepth)
-    {
-        makeTempAns(num);
-        if (diff())
-        {
-            strcpy(max, tempAns);
-        }
+	if (rank == 0)
+	{
+		makeTempAns(numberPointer);
+		if (isTempAnsBig())
+		{
+			strcpy(maxNum, compNum);
+		}
+		return;
+	}
+	else if (position == maxDepth)
+		return;
 
-        return;
-    }
-
-    for (int idx = start; idx < maxDepth; idx++)
-    {
-        cutPosition[idx] = 1;
-        dept(num, idx + 1, rank + 1);
-        cutPosition[idx] = 0;
-    }
+	cutPosition[index] = position;
+	dept(index+1, rank - 1, position +1);
+	dept(index, rank, position + 1);
 }
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
-char *solution(const char *number, int k)
+char* solution(const char* number, int k)
 {
-    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    maxDepth = strlen(number);
-    targetDepth = k;
+	// return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+	maxDepth = strlen(number);
+	ansLength = maxDepth - targetDepth;
+	numberPointer = number;
+	targetDepth = k;
 
-    dept(number, 0, 0);
+	dept(0, k, 0);
 
-    return max;
+	return maxNum;
 }
